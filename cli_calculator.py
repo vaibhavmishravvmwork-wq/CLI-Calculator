00423579dacf7088553615cf9f42a6fc8cd2fd73
Vaibhav Mishra <vaibhavmishra.vvm.work@gmail.com>
2026-06-06 12:22:26 +0530

    Initial Commit

---
+++ cli_calculator.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def __init__(self):
+        print("Calculator Created Successfully!!\n\n")
+    
+    def start(self):
+        print("Starting the Calculator")
+
+
+
+if __name__ == "__main__":
+    c = Calculator()
+    c.start()
